c9bab27a05aecda26b0a742160b76d664dfdf6d0
PedroHenriqueSLM <pedrolopes07164@gmail.com>
2023-01-20 15:27:21 -0300

    Criação da classe startup e conexão com banco de dados

---
+++ WebPortal/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebPortal.Models;
+
+namespace WebPortal.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ WebPortal/Migrations/20230120175041_WebPortal.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebPortal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioNome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UsuarioSobrenome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UsuarioTelefone = table.Column<int>(type: "int", nullable: false),
+                    UsuarioEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    ConfirmarSenha = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.UsuarioId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
